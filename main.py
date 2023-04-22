5e88a2f6e3624256ef0d61ca415d6382ad04b856
Kattnos <kattnoskanal@gmail.com>
2023-04-22 22:58:53 +0200

    bs

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 import html2text
+from bs4 import BeautifulSoup
+import lxml
 import tkinter as tk
 from tkinter import messagebox
 
@@ -11,32 +13,30 @@ def updateText():
     url = inputSelect.get()
 
     response = requests.get(url)
-
     html = response.text
-    content = html2text.html2text(html)
+    soup = BeautifulSoup(html, 'lxml')
 
-   # if content.find('<title>') != -1:
-    #    title1 = content.partition('<title>')[2]
-     #   title = title1.partition('</title>')[0]
-    outputField.delete('1.0',tk.END)
-    outputField.insert(tk.END,content)
-    print(content)
-
+    articleTitle = soup.title
+    articleText = soup.get_text()
+    outputField.delete('1.0', tk.END)
+    outputField.insert(tk.END, articleText)
 
 inputField = tk.Frame(root)
 inputSelect = tk.Entry(inputField, width=40)
 inputSelectText = tk.Label(inputField, text='Select URL', font=('Arial', 11))
 confirm = tk.Button(inputField, text='Confirm URL', font=('Arial', 12), command=updateText)
-outputField = tk.Text(root, font=('Arial', 12))
+outputFieldFrame = tk.Frame(root)
+outputField = tk.Text(outputFieldFrame, font=('Arial', 12))
 
 
 confirm.grid(column=1, row=0)
 inputSelect.grid(column=0, row=0, padx=5)
 inputSelectText.grid(column=0, row=1, columnspan=1)
 inputField.pack(pady=5,padx=2)
-outputField.pack(pady=20,padx=10)
+outputFieldFrame.pack(pady=20,padx=10)
+outputField.pack(side =tk.LEFT, fill = tk.BOTH)
 
-scroll = tk.Scrollbar(root, orient='vertical', command=outputField.yview)
+scroll = tk.Scrollbar(outputFieldFrame, orient='vertical', command=outputField.yview)
 outputField.config(yscrollcommand=scroll.set)
 scroll.pack(side=tk.RIGHT, fill='y')
 
